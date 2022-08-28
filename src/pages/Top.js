625a27f3d59b2281b5579ec92c91aa63559c32c7
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-08-28 23:07:16 +0900

    VideoGrid VideoGridItem

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -2,21 +2,40 @@ import React, { useContext, useEffect } from "react";
 import Layout from "../components/layout/Layout";
 import { fetchPopularData } from "../apis/index";
 import { Store } from "../store/index";
+import VideoGrid from "../components/VideoGrid/VideoGrid";
+import VideoGridItem from "../components/VideoGridItem/VideoGridItem";
 
 const Top = () => {
   const { globalState, setGlobalState } = useContext(Store);
 
   useEffect(() => {
     fetchPopularData().then((res) => {
-      console.log("data", res.data.items);
       setGlobalState({
         type: "SET_POPULAR",
         payload: { popular: res.data.items },
       });
     });
   }, []);
+  console.log("globalState: ");
+  console.log(globalState.popular.popular);
 
-  return <Layout>top</Layout>;
+  return (
+    <Layout>
+      <VideoGrid>
+        {globalState.popular.popular &&
+          globalState.popular.popular.map((popular) => {
+            return (
+              <VideoGridItem
+                id={popular.id}
+                key={popular.id}
+                src={popular.snippet.thumbnails.default.url}
+                title={popular.snippet.title}
+              />
+            );
+          })}
+      </VideoGrid>
+    </Layout>
+  );
 };
 
 export default Top;
