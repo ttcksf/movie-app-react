5b2687d470ed487851c19f2fdde6a9d107e5e85a
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-08-29 19:39:11 +0900

    refactoring selectedVideo relatedVideo

--- src/components/SideList/SideList.js
+++ src/components/SideList/SideList.js
@@ -1,23 +1,11 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { Store } from "../../store";
 import Style from "./SideList.module.scss";
-import { fetchRelatedData } from "../../apis/index";
 import SideListItem from "../SideListItem/SideListItem";
 
 const SideList = () => {
-  const { globalState, setGlobalState } = useContext(Store);
-  const setRelatedVideo = async (id) => {
-    await fetchRelatedData(id).then((res) => {
-      setGlobalState({
-        type: "SET_RELATED",
-        payload: { related: res.data.items },
-      });
-    });
-  };
-  //動画を選択するごとに関連動画はレンダリング必要なので第二引数に選択した動画（selected）を指定
-  useEffect(() => {
-    setRelatedVideo(globalState.selected.id);
-  }, [globalState.selected]);
+  const { globalState } = useContext(Store);
+
   return (
     <div className={Style.sidenav}>
       {globalState.related.related ? (
--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -1,27 +1,12 @@
-import React, { useContext, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { fetchSelectedData } from "../../apis";
+import React, { useContext } from "react";
 import { Store } from "../../store/index";
 import VideoPlay from "../VideoPlay/VideoPlay";
 import Style from "./VideoDetail.module.scss";
 import Linkify from "react-linkify";
 
 const VideoDetail = () => {
-  const { globalState, setGlobalState } = useContext(Store);
-  const location = useLocation();
-  const setSelectedVideo = async () => {
-    const searchParams = new URLSearchParams(location.search);
-    const id = searchParams.get("v");
-    await fetchSelectedData(id).then((res) => {
-      //res.data.itemsは配列になっているため、中のオブジェクトを取り出す（中身は1個しかないからshit()でOK）
-      const item = res.data.items.shift();
-      setGlobalState({ type: "SET_SELECTED", payload: { selected: item } });
-    });
-  };
-  //動画の選択時にURLが切り替わるのでレンダリングするように第二引数にURLを設定
-  useEffect(() => {
-    setSelectedVideo();
-  }, [location]);
+  const { globalState } = useContext(Store);
+
   return globalState.selected.selected && globalState.selected.selected.id ? (
     <div className={Style.wrapper}>
       <VideoPlay id={globalState.selected.selected.id} />
